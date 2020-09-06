0798b7cb146bcf362f8581159b22fdab220d17a4
tonykosmos <a.kosmovskiy@gmail.com>
2020-09-06 16:52:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 exports.min = function min (array) {
-  return 0;
+  
+  if (arguments.length > 0 && array.length > 0) return Math.min(...array);
+  return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length > 0 && array.length > 0) return Math.max(...array);
+  return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length > 0 && array.length > 0) return array.reduce((a, b) => a + b) / array.length;
+  return 0
 }
